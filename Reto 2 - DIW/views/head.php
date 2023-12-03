1e2583942363635c7d1a09e8b317268ec11283f1
Pepe <josejoaquin.s331419@cesurformacion.com>
2023-12-03 18:51:26 +0100

    Añadido normalize, corregido style en consecuencia

--- Reto 2 - DIW/views/head.php
+++ Reto 2 - DIW/views/head.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="static/css/style.css">
-    <link href="">
+    <link rel="stylesheet" href="static/css/normalize.css">
     <title><?php echo $titulo; ?></title>
     <?php if (!empty($linkJS)) echo "<script src='static/js/{$linkJS}.js'></script>"; ?>
     <script src="static/js/header.js"></script>
